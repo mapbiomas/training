24457b078ccd5be9ad838a29f09cbdc271739a43
Joao Siqueira <joaosiqueira@MacBook-Air-de-Joao.local>
2025-05-02 14:50:56 -0300

    atualizacao do gap_fill_filter

--- 3_mapbiomas_methodologies/1_lulc_general_map/4_post_classification/2_gap_fill_filter/gap_fill_filter.js
+++ 3_mapbiomas_methodologies/1_lulc_general_map/4_post_classification/2_gap_fill_filter/gap_fill_filter.js
@@ -1,176 +1,227 @@
-// -- -- -- -- 06_gapFill
-// post-processing filter: fill gaps (nodata) with data from previous years
-// barbara.silva@ipam.org.br and dhemerson.costa@ipam.org.br
-// update for Guyanas and Suriname
-// joaquim.pereira@ipam.org.br, emanuel.valero@gaiaamazonas.org
+// ========================
+// Initial Configuration
+// ========================
 
-
-// Set the country biome extent 
+// Define the country or territory name.
+// It must match the name used in the training samples and mosaics.
+// Use uppercase letters, without spaces or underscores.
+// Example: 'SURINAME' for Suriname.
 var territory_name = 'SURINAME';
 
-
 // Define region id
-var region_id = '1';
-
-
-// Define input version
-var input_version = '5';
-
+var region_id = '2';
 
 // Collection ID and version for the stable map.
 var collection_id = 1.0;
-var output_version = '5';
+var input_version = '2';
+var output_version = '2a';
 
-
-// Set the list of years to be filtered
+// List of years to be processed.
 var years = [
     2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007,
     2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015,
     2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023,
 ];
 
-
 // Pattern for naming the exported trained samples.
 // Use '{year}' as a placeholder to be replaced with each processing year.
 // Use '{version}' as a placeholder for the version number.
 // Use '{region_id}' to specify the region ID.
 // Example: 'SURINAME_1_5' for the classification of region with region_id 1 and input_version 1.
-var pattern_name = '{territory_name}_{region_id}_{version}';
-
+var classification_name_pattern = '{territory_name}_{region_id}_{version}';
 
 // Path for input asset
 var input_asset = 'projects/mapbiomas-suriname/assets/LAND-COVER/COLLECTION-1/TRAINING/classification';
 
-
 // Path for the output asset
 var output_asset = 'projects/mapbiomas-suriname/assets/LAND-COVER/COLLECTION-1/TRAINING/classification-ft';
 
+// Description of the classification version.
+var classification_version_description = [
+    "### Classification Version Description",
+    "- **Description**: Apply gap fill filter to improve land cover classification.",
+    "- **Classification Version**: {input_version}",
+    "- **Filtered Version**: {output_version}",
+    "- **Region ID**: {region_id}",
+];
+
+// Color palette for each land cover class
+var palette = [
+    '#ffffff', // 0 - no data
+    '#000000', // 1
+    '#000000', // 2
+    '#1f8d49', // 3 - forest
+    '#000000', // 4
+    '#000000', // 5
+    '#000000', // 6
+    '#000000', // 7
+    '#000000', // 8
+    '#000000', // 9
+    '#000000', // 10
+    '#519799', // 11 - wetland
+    '#d6bc74', // 12 - grassland
+    '#000000', // 13
+    '#000000', // 14
+    '#000000', // 15
+    '#000000', // 16
+    '#000000', // 17
+    '#000000', // 18
+    '#000000', // 19
+    '#000000', // 20
+    '#ffefc3', // 21 - mosaic_of_uses
+    '#000000', // 22
+    '#000000', // 23
+    '#000000', // 24
+    '#db4d4f', // 25 - non_vegetated_area
+    '#000000', // 26
+    '#000000', // 27
+    '#000000', // 28
+    '#000000', // 29
+    '#000000', // 30
+    '#000000', // 31
+    '#000000', // 32
+    '#2532e4', // 33 - water
+];
+
+// Visualization parameters for the map
+var vis = {
+    min: 0,
+    max: 33,
+    palette: palette,
+    format: 'png',
+};
 
 // Set input classification
-var input_path = input_asset + '/' + pattern_name
+var input_path = input_asset + '/' + classification_name_pattern
     .replace('{territory_name}', territory_name)
     .replace('{region_id}', region_id)
     .replace('{version}', input_version);
 
+// Load the classification image
+var classification = ee.Image(input_path);
+print('Input classification', classification);
 
-var input_classification = ee.Image(input_path);
-print('Input classification', input_classification);
+// Index of each year in the list of years
+var years_index = ee.List.sequence(0, years.length - 1);
 
+/**
+ * @description 
+ * Function to fill the missing values in the classification image.
+ * @param current_year_index 
+ * @param obj - The object containing the previous year image and the current year image.
+ * @returns {Object} 
+ */
+var gap_fill = function (current_year_index, obj) {
+    
+    obj = ee.Dictionary(obj);
 
-// User defined functions
-var applyGapFill = function (image) {
+    // Get the previous year index as an integer
+    var previous_year_index = ee.Number(current_year_index).subtract(1);
 
-    // apply the gapfill from t0 until tn
-    var imageFilledt0tn = bandNames.slice(1)
-        .iterate(
-            function (bandName, previousImage) {
+    // Get the previous image with all the bands
+    var image_filled = ee.Image(obj.get('image_filled'));
+    // Get the image base
+    var image_base = ee.Image(obj.get('image_base'));
 
-                var currentImage = image.select(ee.String(bandName));
+    // Get the current year image
+    var current_image = image_base.select([ee.Number(current_year_index)]);
+    // Get the previous year image
+    var previous_image_filled = image_filled.select([previous_year_index]);
 
-                previousImage = ee.Image(previousImage);
+    // fill the missing values in the current year image
+    var current_filled = current_image.unmask(previous_image_filled);
 
-                currentImage = currentImage.unmask(
-                    previousImage.select([0]));
-
-                return currentImage.addBands(previousImage);
-
-            }, ee.Image(imageAllBands.select([bandNames.get(0)]))
-        );
-
-    imageFilledt0tn = ee.Image(imageFilledt0tn);
-
-    // apply the gapfill from tn until t0
-    var bandNamesReversed = bandNames.reverse();
-
-    var imageFilledtnt0 = bandNamesReversed.slice(1)
-        .iterate(
-            function (bandName, previousImage) {
-
-                var currentImage = imageFilledt0tn.select(ee.String(bandName));
-
-                previousImage = ee.Image(previousImage);
-
-                currentImage = currentImage.unmask(
-                                previousImage.select(previousImage.bandNames().length().subtract(1)));
-
-                return previousImage.addBands(currentImage);
-
-            }, ee.Image(imageFilledt0tn.select([bandNamesReversed.get(0)]))
-        );
-
-    imageFilledtnt0 = ee.Image(imageFilledtnt0).select(bandNames);
-
-    return imageFilledtnt0;
+    // set the object with the previous year image
+    // and the current year image
+    
+    obj = obj.set('image_filled', image_filled.addBands(current_filled));
+    
+    return obj;
 };
 
-// Get band names list 
-var bandNames = ee.List(
-    years.map(
-        function (year) {
-            return 'classification_' + String(year);
-        }
-    )
-);
+/**
+ * @description
+ * Function to forward fill the missing values in the classification image.
+ * It uses the previous year to fill the current year.
+ * @param image - The classification image to be filled.
+ * @returns {ee.Image} - The filled classification image.
+ */
+var forward_fill = function (image) {
+    // Get the first year image
+    var first = image.select([0]);
 
-// Generate a histogram dictionary of [bandNames, image.bandNames()]
-var bandsOccurrence = ee.Dictionary(
-    bandNames.cat(input_classification.bandNames()).reduce(ee.Reducer.frequencyHistogram())
-);
+    // Iterate to fill missing bands
+    var result = years_index.slice(1).iterate(gap_fill, {
+        image_filled: first,
+        image_base: image
+    });
 
-// Insert a masked band 
-var bandsDictionary = bandsOccurrence.map(
-    function (key, value) {
-        return ee.Image(
-            ee.Algorithms.If(
-                ee.Number(value).eq(2),
-                input_classification.select([key]).byte(),
-                ee.Image().rename([key]).byte().updateMask(input_classification.select(0))
-            )
-        );
-    }
-);
-
-// Convert dictionary to image
-var imageAllBands = ee.Image(
-    bandNames.iterate(
-        function (band, image) {
-            return ee.Image(image).addBands(bandsDictionary.get(ee.String(band)));
-        },
-        ee.Image().select()
-    )
-);
-
-// Apply the gapfill function
-var imageFilledtnt0 = applyGapFill(imageAllBands);
+    // Extract the final result (all bands)
+    return ee.Image(ee.Dictionary(result).get('image_filled'));
+};
 
 
-// Add filtered image to map
-print('Output classification', imageFilledtnt0);
-var palette = require('users/mapbiomas/modules:Palettes.js').get('classification8');
-var vis = { min: 0, max: 62, palette: palette };
-Map.addLayer(input_classification.select(['classification_2022']), vis, 'Input classification');
-Map.addLayer(imageFilledtnt0.select('classification_2022'), vis, 'filtered');
+/**
+ * @description
+ * Function to backward fill the missing values in the classification image.
+ * It uses the previous year to fill the current year.
+ * @param image - The classification image to be filled.
+ * @returns {ee.Image} - The filled classification image.
+ */
+var backward_fill = function (image) {
+    // Reverse the order of the bands
+    image = image.select(years_index.reverse());
 
-// Write metadata
-imageFilledtnt0 = imageFilledtnt0
+    // Get the last year image
+    var last = image.select([0]);
+
+    // Iterate to fill missing bands
+    // The first band is the last year, so we start from the second band
+    // and iterate backwards
+    var result = years_index.slice(1).iterate(gap_fill, {
+        image_filled: last,
+        image_base: image
+    });
+
+    // Extract the final result (all bands)
+    // Reverse the order of the bands back to the original
+    return ee.Image(ee.Dictionary(result).get('image_filled')).select(years_index.reverse());
+};
+
+// Apply the forward gap fill
+var forward_filled = forward_fill(classification.selfMask());
+
+// Apply the backward fill
+var fully_filled = backward_fill(forward_filled);
+
+print('output classification', fully_filled);
+
+// Write metadata to the output classification (important for tracking and exporting)
+var classification_filtered = fully_filled
+    .set('description', classification_version_description.join('\n'))
     .set('collection_id', collection_id)
     .set('version', output_version)
     .set('territory', territory_name)
-    .set('step', 'gapfill');
+    .set('step', 'frequency_filter');
 
-// Define output path name
-var output_name = pattern_name
+// Define output path name based on the naming pattern
+var output_name = classification_name_pattern
     .replace('{territory_name}', territory_name)
     .replace('{region_id}', region_id)
     .replace('{version}', output_version);
 
-// Export as GEE asset
+// Export the processed classification image as a GEE asset
 Export.image.toAsset({
-    image: imageFilledtnt0,
+    image: classification_filtered,
     description: output_name,
     assetId: output_asset + '/' + output_name,
-    pyramidingPolicy: {'.default': 'mode'},
-    region: imageFilledtnt0.geometry().bounds(),
+    pyramidingPolicy: { '.default': 'sample' },
+    region: classification.geometry().bounds(),
     scale: 30,
     maxPixels: 1e13
 });
+
+vis['bands'] = ['classification_2020'];
+
+Map.addLayer(classification, vis, 'classification', false);
+Map.addLayer(classification_filtered, vis, 'classification gap filled', false);
